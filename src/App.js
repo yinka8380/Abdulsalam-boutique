66173bee3af9f08b4dd6f973d9f70a9c6f81e632
@Abdulsalam <yinka8380@gmail.com>
2025-06-01 17:16:56 +0100

    stage all the file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './homepage';
+import './App.css';
+function App() {
+  return (
+     <div>
+      <Homepage />
+    </div>
+
+   
+  );
+}
+
+export default App;
